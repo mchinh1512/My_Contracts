600cce7e742ec586071766bbbd88071d99690d04
mchinh1512 <mchinh1512@gmail.com>
2022-06-16 11:11:25 +0700

    update

--- contracts/ERC20TransferProxy.sol
+++ contracts/ERC20TransferProxy.sol
@@ -12,4 +12,11 @@ contract ERC20TransferProxy is IERC20TransferProxy, Initializable, OperatorRole 
     function erc20safeTransferFrom(IERC20Upgradeable token, address from, address to, uint256 value) override external onlyOperator {
         require(token.transferFrom(from, to, value), "failure while transferring");
     }
+
+    function VerifyToken(bytes32 _hashedToken, uint8 _v, bytes32 _r, bytes32 _s) public pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Token:\n32";
+        bytes32 prefixedHashToken = keccak256(abi.encodePacked(prefix, _hashedToken));
+        address signer = ecrecover(prefixedHashToken, _v, _r, _s);
+        return signer;
+    }
 }
